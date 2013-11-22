57bac3c1b909584f0ef4f7ec55ed85999d2996ef
Thies C. Arntzen <thieso@gmail.com>
2013-11-22 13:21:16 +0100

    detect and build local librrdiff

--- ext/rrdiff/extconf.rb
+++ ext/rrdiff/extconf.rb
@@ -1,6 +1,29 @@
 require 'mkmf'
 
-fail unless have_library('rsync')
+unless File.exists? "librsync-0.9.7/librsync.la"
+  `cd librsync-0.9.7; ./configure --enable-static --disable-shared && make librsync.la`
+end
+
+LIBDIR      = RbConfig::CONFIG['libdir']
+INCLUDEDIR  = RbConfig::CONFIG['includedir']
+
+HEADER_DIRS = [
+  'librsync-0.9.7',
+  '/usr/local/include',
+  INCLUDEDIR,
+  '/usr/include',
+]
+
+LIB_DIRS = [
+  'librsync-0.9.7/.libs',
+  '/usr/local/lib',
+  LIBDIR,
+  '/usr/lib',
+]
+
+dir_config('rrdiff', HEADER_DIRS, LIB_DIRS)
+
+have_library('rsync')
 have_header('librsync.h')
 
 create_makefile('rrdiff/rrdiff')
