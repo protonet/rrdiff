910b6b71da97b9a707bea9afb0839b02a453edbf
Thies C. Arntzen <thieso@gmail.com>
2013-11-22 13:39:24 +0100

    fix

--- ext/rrdiff/extconf.rb
+++ ext/rrdiff/extconf.rb
@@ -4,27 +4,7 @@ unless File.exists? "librsync-0.9.7/librsync.la"
   `cd librsync-0.9.7; ./configure --enable-static --disable-shared && make librsync.la`
 end
 
-LIBDIR      = RbConfig::CONFIG['libdir']
-INCLUDEDIR  = RbConfig::CONFIG['includedir']
-
-$LDFLAGS << "librsync-0.9.7/.libs/librsync.a"
-
-HEADER_DIRS = [
-  'librsync-0.9.7',
-  '/usr/local/include',
-  INCLUDEDIR,
-  '/usr/include',
-]
-
-LIB_DIRS = [
-  'librsync-0.9.7/.libs',
-  '/usr/local/lib',
-  LIBDIR,
-  '/usr/lib',
-]
-
-dir_config('rrdiff', HEADER_DIRS, LIB_DIRS)
-
-have_header('librsync.h')
+$CFLAGS << " -Ilibrsync-0.9.7 "
+$LDFLAGS << " #{File.dirname(__FILE__)}/librsync-0.9.7/.libs/librsync.a "
 
 create_makefile('rrdiff/rrdiff')
