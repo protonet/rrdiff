81a4463a44e09090a0d60a252bfb942f44305e7e
protonet <protonet@localhost>
2013-12-05 11:15:58 +0100

    use librsync if installed

--- ext/rrdiff/extconf.rb
+++ ext/rrdiff/extconf.rb
@@ -1,10 +1,14 @@
 require 'mkmf'
 
-unless File.exists? "librsync-0.9.7/librsync.la"
-  `cd librsync-0.9.7; ./configure --enable-static --disable-shared && make librsync.la`
-end
+if have_library('rsync')
+  have_header('librsync.h')
+else
+  unless File.exists? "librsync-0.9.7/librsync.la"
+    `cd librsync-0.9.7; ./configure --enable-static --disable-shared && make librsync.la`
+  end
 
-$CFLAGS << " -Ilibrsync-0.9.7 "
-$LDFLAGS << " #{File.dirname(__FILE__)}/librsync-0.9.7/.libs/librsync.a "
+  $CFLAGS << " -Ilibrsync-0.9.7 "
+  $LDFLAGS << " #{File.dirname(__FILE__)}/librsync-0.9.7/.libs/librsync.a "
+end
 
 create_makefile('rrdiff/rrdiff')
