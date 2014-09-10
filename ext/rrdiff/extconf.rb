105b9329c80b334779f4a87a6dd9f5082ae8940c
Thies C. Arntzen <thieso@gmail.com>
2014-09-10 14:08:39 +0200

    use installed librdiff

--- ext/rrdiff/extconf.rb
+++ ext/rrdiff/extconf.rb
@@ -1,10 +1,14 @@
 require 'mkmf'
 
-unless File.exists? "librsync-0.9.7/librsync.la"
-  `cd librsync-0.9.7; ./configure --enable-static --disable-shared && make librsync.la`
+if have_library('rsync')
+  have_header('librsync.h')
+else
+  unless File.exists? "librsync-0.9.7/librsync.la"
+    `cd librsync-0.9.7; ./configure --enable-static --disable-shared && make librsync.la`
+  end
+ 
+  $CFLAGS << " -Ilibrsync-0.9.7 "
+  $LDFLAGS << " #{File.dirname(__FILE__)}/librsync-0.9.7/.libs/librsync.a "
 end
-
-$CFLAGS << " -Ilibrsync-0.9.7 "
-$LDFLAGS << " #{File.dirname(__FILE__)}/librsync-0.9.7/.libs/librsync.a "
 
 create_makefile('rrdiff/rrdiff')
